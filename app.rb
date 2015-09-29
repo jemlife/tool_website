22fed1c496558356eff070959860031ecbe4dcab
Scott Jansen <scott.jansen731@outlook.com>
2015-09-28 17:53:22 -0700

    Add form page to get info from user.

--- app.rb
+++ app.rb
@@ -7,24 +7,19 @@ end
 
 get('/new_song') do
   erb(:song)
-  end
+end
 
 
 get('/photos') do
-	"<!DOCTYPE html>
-  <html>
-  <head>
-    <title>TOOL ART</title>
-    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
-  </head>
-  <body>
-    <h1>Tool's Dionysian Art</h1>
-     <ul>
-        <li><img src='/img/alexgray.jpg' alt='A photo of art.'/></li>
-        <li><img src='/img/face.jpg' alt='A photo of art.'/></li>
-        <li><img src='/img/tool.jpg' alt='A photo of art.'/></li>
-        <li><img src='/img/toolband.jpg' alt='A photo of the band'/></li>
-      </ul>
-  </body>
-  </html"
-	end
+  erb(:art)
+end
+
+get('/purchase_form') do
+	erb(:form) 
+end
+
+get('/reciept') do
+	@recipient = params.fetch('recipient')
+	@tickets = params.fetch('tickets')
+	erb(:reciept)
+end
